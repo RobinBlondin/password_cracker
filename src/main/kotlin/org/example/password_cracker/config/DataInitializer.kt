da3b69ec13b9c29ba0cbe739b37370a6818dbb56
Robin Blondin <robin.blondin@protonmail.com>
2024-08-22 13:49:22 +0200

    mocking users at start up

---
+++ src/main/kotlin/org/example/password_cracker/config/DataInitializer.kt
@@ -0,0 +1,30 @@
+package org.example.password_cracker.config
+
+import org.example.password_cracker.enums.Role
+import org.example.password_cracker.model.User
+import org.example.password_cracker.repository.UserRepository
+import org.springframework.boot.ApplicationRunner
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import java.util.*
+
+@Configuration
+class DataInitializer {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    @Bean
+    fun init(userRepository: UserRepository, passwordEncoder: PasswordEncoder) = ApplicationRunner {
+
+        if(userRepository.findUserByEmail("admin@cracker.se").isPresent) {
+            return@ApplicationRunner
+        }
+
+        val admin = User(id = UUID.randomUUID(), email = "admin@cracker.se", password = passwordEncoder.encode("password"), role = Role.ADMIN)
+        val client = User(id = UUID.randomUUID(), email = "client@cracker.se", password = passwordEncoder.encode("password"), role = Role.CLIENT)
+        userRepository.saveAll(listOf(admin, client))
+    }
+}
