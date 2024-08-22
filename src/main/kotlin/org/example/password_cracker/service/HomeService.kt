18a490a027e05b680a71bc4357804c0d029b1998
Robin Blondin <robin.blondin@protonmail.com>
2024-08-23 01:25:56 +0200

    Added hashing methods

--- src/main/kotlin/org/example/password_cracker/service/HomeService.kt
+++ src/main/kotlin/org/example/password_cracker/service/HomeService.kt
@@ -7,7 +7,22 @@ import java.io.File
 
 @Service
 class HomeService {
-    private fun hashPassword(password: String): String = BCryptPasswordEncoder().encode(password)
+    val sha256Map = mutableMapOf<String, String>()
+    val md5Map = mutableMapOf<String, String>()
+
+    private fun hashPassword(password: String): Pair<String, String> {
+        val sha = MessageDigest.getInstance("SHA-256")
+        val md = MessageDigest.getInstance("MD5")
+        val shaHash = sha.digest(password.toByteArray()).fold("") { str, it -> str + "%02x".format(it) }
+        val mdHash = md.digest(password.toByteArray()).fold("") { str, it -> str + "%02x".format(it) }
+
+        return Pair(shaHash, mdHash)
+    }
+
+    fun saveEntryToFile(password: String): Pair<String, String> {
+        val hashes = hashPassword(password)
+        sha256Map[password] = hashes.first
+        md5Map[password] = hashes.second
 
     fun saveEntryToFile(password: String): String {
         val hashedPassword = hashPassword(password)
