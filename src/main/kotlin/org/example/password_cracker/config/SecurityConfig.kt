81865bf7c296493df547908622f100e969cf4cd7
Robin Blondin <robin.blondin@protonmail.com>
2024-08-22 19:50:10 +0200

    Adjusted security config for better authorization

--- src/main/kotlin/org/example/password_cracker/config/SecurityConfig.kt
+++ src/main/kotlin/org/example/password_cracker/config/SecurityConfig.kt
@@ -2,26 +2,36 @@ package org.example.password_cracker.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Lazy
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig(
     private val customUserDetailsService: CustomUserDetailsService,
-    private val customAuthenticationProvider: CustomAuthenticationProvider) {
+    @Lazy private val customAuthenticationProvider: CustomAuthenticationProvider) {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 
     @Bean
      fun configure(http: HttpSecurity): SecurityFilterChain =
         http
+            .csrf { csrf ->
+                csrf.disable()
+            }
             .authenticationProvider(customAuthenticationProvider)
             .authorizeHttpRequests { auth ->
                 auth
-                    .requestMatchers("/home", "/crack").hasAnyRole("USER", "ADMIN")
+                    .requestMatchers("/", "/login").permitAll()
+                    .requestMatchers("/home", "/crack").authenticated()
                     .anyRequest().permitAll()
             }
             .formLogin { formLogin ->
@@ -29,7 +39,7 @@ class SecurityConfig(
                     .loginPage("/login")
                     .loginProcessingUrl("/login")
                     .defaultSuccessUrl("/home", true)
-                    .failureUrl("/?error=true")
+                    .failureUrl("/login?error=true")
                     .permitAll()
             }
             .logout { logout ->
