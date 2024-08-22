87eb800e68823798f19beef5f504be9fef7fb748
Robin Blondin <robin.blondin@protonmail.com>
2024-08-22 19:47:03 +0200

    Changed exception type

--- src/main/kotlin/org/example/password_cracker/config/CustomAuthenticationProvider.kt
+++ src/main/kotlin/org/example/password_cracker/config/CustomAuthenticationProvider.kt
@@ -1,29 +1,35 @@
 package org.example.password_cracker.config
 
+import org.springframework.context.annotation.Lazy
 import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 
 @Component
 class CustomAuthenticationProvider(
-    private val userDetailsService: CustomUserDetailsService,
-    private val passwordEncoder: PasswordEncoder
+    @Lazy private val userDetailsService: CustomUserDetailsService,
+    @Lazy private val passwordEncoder: PasswordEncoder
 ): AuthenticationProvider {
     override fun authenticate(authentication: Authentication): Authentication {
+
         val username = authentication.name
         val password = authentication.credentials.toString()
-
         val userDetails = userDetailsService.loadUserByUsername(username)
 
         return if (passwordEncoder.matches(password, userDetails.password)) {
-            UsernamePasswordAuthenticationToken(username, password, userDetails.authorities)
+            UsernamePasswordAuthenticationToken(userDetails, password, userDetails.authorities)
         } else {
-            throw Exception("Invalid credentials")
+            throw BadCredentialsException("Invalid credentials")
         }
     }
 
-    override fun supports(authentication: Class<*>?): Boolean = UsernamePasswordAuthenticationToken::class.java.isAssignableFrom(authentication!!)
+    override fun supports(authentication: Class<*>?): Boolean =
+        UsernamePasswordAuthenticationToken::class.java.isAssignableFrom(authentication!!)
+
 
 }
