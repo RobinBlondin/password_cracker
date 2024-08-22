699b59e5c27ad9687bfed4806b9cf7cb3bd6bed7
Robin Blondin <robin.blondin@protonmail.com>
2024-08-23 01:27:19 +0200

    created controller logic for getting and redirecting hashes and passwords

--- src/main/kotlin/org/example/password_cracker/controller/HomeController.kt
+++ src/main/kotlin/org/example/password_cracker/controller/HomeController.kt
@@ -1,10 +1,8 @@
 package org.example.password_cracker.controller
 
-import lombok.RequiredArgsConstructor
 import org.example.password_cracker.service.HomeService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -20,23 +18,29 @@ class HomeController(@Autowired private val homeService: HomeService) {
     fun index(): String = "login"
 
     @GetMapping("/home")
-    fun home(@RequestParam("result", required = false) result: String?, model: Model): String {
-        if(result != null) {
-            model.addAttribute("result", result)
-        }
-        return "home"
-    }
+    fun home(): String = "home"
 
     @PostMapping("/home")
     fun homePost(@RequestParam("search") input: String, rda: RedirectAttributes): String {
-        val result = homeService.saveEntryToFile(input)
-        rda.addAttribute("result", result)
+        if(input.isNotEmpty()) {
+            val result = homeService.saveEntryToFile(input)
+            rda.addFlashAttribute("sha256", result.first)
+            rda.addFlashAttribute("md5", result.second)
+        }
+
         return "redirect:/home"
     }
 
 
     @GetMapping("/crack")
     fun crack(): String = "crack"
+
+    @PostMapping("/crack")
+    fun crackPost(@RequestParam("search") hash: String, rda: RedirectAttributes): String {
+        val result = homeService.crackHash(hash)
+        rda.addFlashAttribute("result", result)
+        return "redirect:/crack"
+    }
 
     @GetMapping("/logout")
     fun logout(): String = "redirect:/login"
