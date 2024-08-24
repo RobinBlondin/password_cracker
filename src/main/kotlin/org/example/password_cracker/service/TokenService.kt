6e17176c68a5de65d33cb9b429243e3ca1021d3b
Robin Blondin <robin.blondin@protonmail.com>
2024-08-25 00:17:08 +0200

    Created token service with methods

---
+++ src/main/kotlin/org/example/password_cracker/service/TokenService.kt
@@ -0,0 +1,31 @@
+package org.example.password_cracker.service
+
+
+import org.example.password_cracker.model.Token
+import org.example.password_cracker.model.User
+import org.example.password_cracker.repository.TokenRepository
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class TokenService(val tokenRepository: TokenRepository) {
+    fun findToken(token: String): Optional<Token> {
+        return tokenRepository.findByToken(token)
+    }
+
+    fun deleteToken(token: String) {
+        tokenRepository.findByToken(token).ifPresent { tokenRepository.delete(it) }
+    }
+
+    fun deleteAllExpiredTokens() {
+        tokenRepository.findAll().forEach {
+            if(it.isExpired()) {
+                tokenRepository.delete(it)
+            }
+        }
+    }
+
+    fun save(token: Token) {
+        tokenRepository.save(token)
+    }
+}
