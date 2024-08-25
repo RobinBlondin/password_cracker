387045109e265d2ec5dbc362d90d3e1a2e53ab18
Robin Blondin <robin.blondin@protonmail.com>
2024-08-25 10:19:42 +0200

    changed table name to avoid syntax conflict in SQL

--- src/main/kotlin/org/example/password_cracker/model/User.kt
+++ src/main/kotlin/org/example/password_cracker/model/User.kt
@@ -14,6 +14,7 @@ import java.util.UUID
 @NoArgsConstructor
 @Builder
 @Entity
+@Table(name = "_user")
 class User(
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -39,4 +40,5 @@ class User(
     override fun isCredentialsNonExpired(): Boolean = true
 
     override fun isEnabled(): Boolean = enabled
+
 }
