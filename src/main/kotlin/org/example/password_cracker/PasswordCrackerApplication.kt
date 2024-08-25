b984bd3643834a88746eccf6efb1966ba2b52366
Robin Blondin <robin.blondin@protonmail.com>
2024-08-25 10:17:45 +0200

    Manually loading env variables to properties

--- src/main/kotlin/org/example/password_cracker/PasswordCrackerApplication.kt
+++ src/main/kotlin/org/example/password_cracker/PasswordCrackerApplication.kt
@@ -1,11 +1,17 @@
 package org.example.password_cracker
 
+import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class PasswordCrackerApplication
 
 fun main(args: Array<String>) {
+	val dotenv = Dotenv.load()  // Load the environment variables from .env file
+	dotenv.entries().forEach { entry ->
+		System.setProperty(entry.key, entry.value)  // Set each .env entry as a system property
+	}
 	runApplication<PasswordCrackerApplication>(*args)
 }
