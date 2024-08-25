791720f5b9d3d2e8b50ea24724a5756f9df6840f
Robin Blondin <robin.blondin@protonmail.com>
2024-08-25 10:12:18 +0200

    Broke out auth logic to service layer

--- src/main/kotlin/org/example/password_cracker/controller/AuthController.kt
+++ src/main/kotlin/org/example/password_cracker/controller/AuthController.kt
@@ -1,9 +1,8 @@
 package org.example.password_cracker.controller
 
+import jakarta.servlet.http.HttpServletRequest
 import org.example.password_cracker.dto.registerDTO
-import org.example.password_cracker.service.EmailService
-import org.example.password_cracker.service.TokenService
-import org.example.password_cracker.service.UserService
+import org.example.password_cracker.service.AuthService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -13,57 +12,28 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.servlet.mvc.support.RedirectAttributes
 
 @Controller
-class AuthController(val userService: UserService, val emailService: EmailService, val tokenService: TokenService) {
+class AuthController(val authService: AuthService) {
     @GetMapping("/")
     fun root(): String = "redirect:/login"
 
     @GetMapping("/login")
-    fun index(): String = "login"
+    fun index(request: HttpServletRequest, model: Model): String {
+        val error = request.getParameter("error")
+        if(error != null) {
+            model.addAttribute("error_message", error)
+        }
+        return "login"
+    }
 
     @GetMapping("/register")
-    fun register(model: Model): String {
-        if(!model.containsAttribute("password")) {
-            model.addAttribute("password", false)
-        } else if(!model.containsAttribute("email")) {
-            model.addAttribute("email", false)
-        }
-        model.addAttribute("dto", registerDTO())
-        return "register"
-    }
+    fun register(model: Model): String = authService.loadRegisterPageModelAttributes(model)
+
 
     @PostMapping("/register")
-    fun registerPost(@ModelAttribute dto: registerDTO, rda:RedirectAttributes): String {
-        rda.addFlashAttribute("dto", dto)
+    fun registerPost(@ModelAttribute dto: registerDTO, rda:RedirectAttributes): String = authService.registerProcess(rda, dto)
 
-        if(dto.password != dto.confirmPassword) {
-            rda.addFlashAttribute("password", true)
-            return "redirect:/register"
-        } else if (userService.findUserByEmail(dto.email!!).isPresent) {
-            rda.addFlashAttribute("email", true)
-            return "redirect:/register?email=true"
-        } else {
-            val token = userService.register(dto)
-            emailService.sendVerificationEmail(dto.email!!, token!!)
-            return "redirect:/login?register=true"
-        }
-    }
 
     @GetMapping("/verify/{tokenId}")
-    fun verify(@PathVariable tokenId: String): String {
-        val token = tokenService.findToken(tokenId)
+    fun verify(@PathVariable tokenId: String): String = authService.verificationProcess(tokenId)
 
-        if(token.isEmpty) {
-            return "redirect:/login?notFound=true"
-        } else if(token.get().isExpired()) {
-            tokenService.deleteToken(tokenId)
-            userService.deleteUser(token.get().user!!)
-            return "redirect:/login?expired=true"
-        } else {
-            val user = token.get().user
-            user!!.enabled = true
-            userService.save(user)
-            tokenService.deleteToken(tokenId)
-            return "redirect:/login?verified=true"
-        }
-    }
 }
---
+++ src/main/kotlin/org/example/password_cracker/service/AuthService.kt
@@ -0,0 +1,63 @@
+package org.example.password_cracker.service
+
+import org.example.password_cracker.dto.registerDTO
+import org.example.password_cracker.model.Token
+import org.springframework.stereotype.Service
+import org.springframework.ui.Model
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
+
+@Service
+class AuthService(val tokenService: TokenService, val userService: UserService, val emailService: EmailService) {
+    fun loadRegisterPageModelAttributes(model: Model): String {
+        if(!model.containsAttribute("password")) {
+            model.addAttribute("password", false)
+        } else if(!model.containsAttribute("email")) {
+            model.addAttribute("email", false)
+        }
+        model.addAttribute("dto", registerDTO())
+        return "register"
+    }
+
+    fun registerProcess(rda: RedirectAttributes, dto: registerDTO): String {
+        rda.addFlashAttribute("dto", dto)
+
+        if(dto.password != dto.confirmPassword) {
+            rda.addFlashAttribute("password", true)
+            return "redirect:/register"
+        } else if (userService.findUserByEmail(dto.email!!).isPresent) {
+            rda.addFlashAttribute("email", true)
+            return "redirect:/register?email=true"
+        } else {
+            val token = register(dto)
+            emailService.sendVerificationEmail(dto.email!!, token!!)
+            return "redirect:/login?register=true"
+        }
+    }
+
+    fun register(dto: registerDTO): String? {
+        val user = userService.dtoTOUser(dto)
+        val token = Token(user = user)
+
+        userService.save(user)
+        tokenService.save(token)
+        return token.token
+    }
+
+    fun verificationProcess(tokenId: String): String {
+        val token = tokenService.findToken(tokenId)
+
+        if(token.isEmpty) {
+            return "redirect:/login?notFound=true"
+        } else if(token.get().isExpired()) {
+            tokenService.deleteToken(tokenId)
+            userService.deleteUser(token.get().user!!)
+            return "redirect:/login?expired=true"
+        } else {
+            val user = token.get().user
+            user!!.enabled = true
+            userService.save(user)
+            tokenService.deleteToken(tokenId)
+            return "redirect:/login?verified=true"
+        }
+    }
+}
